451e3864bc2899b4e8e34211a4461be97b0087ac
mustafa şentürk <72344505+mdagcan@users.noreply.github.com>
2024-11-25 06:48:43 -0600

    factorialMethod
    factorialMethod

---
+++ src/FaktorialS.java
@@ -0,0 +1,12 @@
+public class FaktorialS {
+    public static long factorial(long n) {
+        if (n == 1)
+            return 1;
+        else
+            return (n * factorial(n - 1));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
